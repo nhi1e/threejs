18d4f3335687076fe578fe0e2eb4c2dc53de8315
nhi1e <tnhile2004@gmail.com>
2024-06-17 11:58:26 -0700

    fin

--- 14-Particles/src/script.js
+++ 14-Particles/src/script.js
@@ -18,15 +18,49 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/9.png')
 
 /**
- * Test cube
+ * Particles
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 50000
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for(let i = 0; i < count*3; i++)
+{
+    positions[i]=(Math.random()-0.5)*10
+    colors[i]=Math.random()
+}
+
+particlesGeometry.setAttribute(
+    'position', 
+    new THREE.BufferAttribute(positions, 3)
 )
-scene.add(cube)
+
+particlesGeometry.setAttribute(
+    'color', 
+    new THREE.BufferAttribute(colors, 3)
+)
+
+const particleMaterial = new THREE.PointsMaterial()
+particleMaterial.size = 0.1
+particleMaterial.sizeAttenuation = true
+// particleMaterial.color = new THREE.Color('#ff00ff')
+particleMaterial.transparent = true
+particleMaterial.alphaMap = particleTexture
+// particleMaterial.alphaTest = 0.001
+// particleMaterial.depthTest = false
+particleMaterial.depthWrite = false
+particleMaterial.blending = THREE.AdditiveBlending
+particleMaterial.vertexColors = true
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particleMaterial)
+scene.add(particles)
 
 /**
  * Sizes
@@ -80,6 +114,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update particles
+    // particles.rotation.y = elapsedTime * 0.1
+    for (let i = 0; i < count; i++){
+        const i3 = i * 3
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
