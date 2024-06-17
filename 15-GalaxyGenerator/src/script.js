ab8cc01f55db711f094df7d2ddf3828d0a0c1d55
nhi1e <tnhile2004@gmail.com>
2024-06-17 12:56:57 -0700

    fin

--- 15-GalaxyGenerator/src/script.js
+++ 15-GalaxyGenerator/src/script.js
@@ -2,6 +2,8 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 
+
+//expand haunted house with galaxy generator as stars on the sky?
 /**
  * Base
  */
@@ -14,14 +16,107 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Galaxy
+const parameters = {}
+parameters.count = 10000
+parameters.size = 0.02
+parameters.radius = 5
+parameters.branches = 3
+parameters.spin = 1
+parameters.randomness = 0.2
+parameters.randomnessPower = 3
+parameters.insideColor = "#ff6030"
+parameters.outsideColor = "#1b3984"
+
+let geometry = null
+let material = null
+let points = null
+
+const generateGalaxy = () => {
+
+    if(points !== null){ //destroy old galaxy
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+    geometry = new THREE.BufferGeometry()
+    const positions = new Float32Array(parameters.count * 3)
+    const colors = new Float32Array(parameters.count * 3)
+
+    const colorInside = new THREE.Color(parameters.insideColor)
+    const colorOutside = new THREE.Color(parameters.outsideColor)
+
+    
+    for(let i = 0; i < parameters.count; i++){
+        const i3 = i * 3
+
+        const radius = Math.random() * parameters.radius
+        const spinAngle = radius * parameters.spin //further away from center, more spin
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2   
+
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1  : -1) 
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1  : -1)
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1  : -1)
+
+
+        positions[i3 + 0] = Math.cos(branchAngle + spinAngle)*radius + randomX  //x
+        positions[i3 + 1] = randomY
+        positions[i3 + 2] = Math.sin(branchAngle + spinAngle)*radius + randomZ //z
+
+        
+        const mixedColor = colorInside.clone()
+        mixedColor.lerp(colorOutside, radius / parameters.radius)
+
+        //color
+        colors[i3 + 0] = mixedColor.r
+        colors[i3 + 1] = mixedColor.g
+        colors[i3 + 2] = mixedColor.b
+
+    }
+
+    geometry.setAttribute(
+        'position', 
+        new THREE.BufferAttribute(positions, 3)
+    )
+    geometry.setAttribute(
+        'color', 
+        new THREE.BufferAttribute(colors, 3)
+    )   
+
+    //material
+    material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true,
+    })
+    //Points
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
+}   
+
+generateGalaxy()
+
+//debug
+gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters,'radius').min(0.1).max(20).step(0.1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+
 /**
  * Test cube
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
 
 /**
  * Sizes
